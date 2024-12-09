979a95c8fcc0e0061722267457d68d524fad8da0
way2masoom <way2masoom@gmail.com>
2024-12-09 13:04:34 +0530

    Learning python

---
+++ LearnignPython/02_addTwoNumber.py
@@ -0,0 +1,11 @@
+#Program to calculate sum of two number
+print("This program only Accept Integer Values")
+
+num1=int(input('Enter the first Number'))
+num2=int(input('Enter the Second Number'))
+
+#Adding the two Number
+sum=int(num1+num2);
+
+#Displaying the sum of two numbers
+print('The sum of {0} and {1} is = {2}'.format(num1,num2,sum))
