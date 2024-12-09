bb0e40922a84ad764d48a6c87e1fe356da68b10d
way2masoom <way2masoom@gmail.com>
2024-12-09 13:19:29 +0530

    Square Root of Number

---
+++ LearnignPython/03_SquareRoot.py
@@ -0,0 +1,10 @@
+#This program to find the Square Root of the given number
+
+#Taking number as Input
+num=input('Enter the Number to find the Square Root : ')
+
+#Logic to find the Square root
+Square=int(num)**0.5;
+
+#Displaying the Square Root
+print('The Square Root of the {0} is = {1}'.format(num,Square));
